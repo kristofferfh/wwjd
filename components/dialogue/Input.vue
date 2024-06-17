6b8cacf23a5a04411765eb7a9c1a36bfb5a04107
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-17 16:50:36 +0200

    Basic input dialog component, needs more styling

--- components/dialogue/Input.vue
+++ components/dialogue/Input.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
-
+defineProps<{
+  author: string
+  image: string
+  message: string
+}>()
 </script>
 <template>
   <div>
-    Input
+    <span id="profile">
+      <h3>{{ author }}</h3>
+      <NuxtImg :src="image" />
+    </span>
+    <span id="message">
+      {{ message }}
+    </span>
   </div>
 </template>
 <style scoped>
+  div {
+    display: flex;
+    width: 100%;
+  }
 
+  #profile img {
+    height: 200px;
+    aspect-ratio: 1;
+    border-radius: 9999px;
+  }
+
+  #message {
+    flex: 1;
+    background-color: var(--bg-chat-input);
+  }
 </style>
