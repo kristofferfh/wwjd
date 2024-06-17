3f3e41b863323cb293ba103c550e6273b9390971
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-17 16:11:34 +0200

    Creating some basic components

---
+++ components/chatbox/Input.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const input = ref()
+
+</script>
+<template>
+  <input type="text" :ref="input">
+  {{ input }}
+</template>
+<style scoped>
+
+</style>
---
+++ components/chatbox/Logo.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <NuxtImg src="jesusbeach.jpg" w/>
+</template>
+<style scoped>
+
+</style>
