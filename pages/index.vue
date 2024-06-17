229d68147bf156f25dad4babb24b3a8c0b12d1c2
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-17 16:12:36 +0200

    Adding new index page

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div id="input">
+    <ChatboxLogo />
+    <ChatboxInput />
+  </div>
+  <div id="output">
+    <DialogueInput />
+    <DialogueOutput />
+  </div>
+</template>
+<style scoped>
+
+</style>
