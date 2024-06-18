5f7dd94bc31782d8046fc83cfa46097e0fb12066
kristoffach <kristofferfh.kodehode@gmail.com>
2024-06-18 16:16:11 +0200

    Placing size constraints on main tag

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,5 +13,15 @@
     justify-content: center;
     align-items: center;
     flex: 1;
+    gap: 2rem;
+
+    min-width: 360px;
+    max-width: 1280px;
   }
+
+  @media only screen and (min-width: 600px) {
+  main {
+    padding: 2rem;
+  }
+}
 </style>
